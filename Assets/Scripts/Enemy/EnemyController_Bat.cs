46445ef435ab69ef3b86737e99261c1c59eda667
Dung Ngo <NVD-Leo>
2024-06-30 12:59:02 +0700

    Dung Lam enemy bat

--- Assets/Scripts/Enemy/EnemyController_Bat.cs
+++ Assets/Scripts/Enemy/EnemyController_Bat.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController_Dung : MonoBehaviour
+public class EnemyController_Bat : MonoBehaviour
 {
     public GameObject pointA;
     public GameObject pointB;
